53594a3027a90033245d71fd324d08dc94aca5be
Stalkerfish <silva.allison2002@gmail.com>
2022-06-13 16:26:53 -0300

    duplicates the code to implement 2 servo motors being controlled with the X Axis of each stick

--- Controle dos Motores/src/main.cpp
+++ Controle dos Motores/src/main.cpp
@@ -1,43 +1,55 @@
-#include <Arduino.h>  //  framework do Arduino
-#include <ESP32Servo.h> //  biblioteca do Servo
+#include <Arduino.h>        //  framework do Arduino
+#include <ESP32Servo.h>     //  biblioteca do Servo
 #include <Ps3Controller.h>  //  biblioteca do Controle de PS3
 
-Servo myservo;
+Servo myservoL;
+Servo myservoR;
 
-int leftXAxis;  //  armazena o valor do eixo X do analógico esquerdo
+int leftXAxis;   //  armazena o valor do eixo X do analógico esquerdo
+int rightXAxis;  //  armazena o valor do eixo X do analógico direito
 
 void setup() {
     Serial.begin(9600);
-    Ps3.begin("94:b9:7e:d5:ab:e2"); //  inicializa o controle que possui o endereço MAC especificado no parâmetro
+    Ps3.begin("94:b9:7e:d5:ab:e2");  //  inicializa o controle que possui o endereço MAC especificado no parâmetro
 
-    myservo.attach(27); //  inicializa o Servo Motor na porta digital 27
-    myservo.write(2); //  e escreve um ângulo de 2 graus nele
+    myservoL.attach(27);  //  inicializa o Servo Motor na porta digital 27
+    myservoR.attach(26);  //  inicializa o Servo Motor na porta digital 27
+
+    myservoL.write(2);  //  e escreve um ângulo de 2 graus nele
+    myservoR.write(2);  //  e escreve um ângulo de 2 graus nele
 
     Serial.println("Ready.");
 }
 
 void loop() {
+    leftXAxis = map(Ps3.data.analog.stick.lx, -128, 127, 2, 181);  // transforma os valores do analógico para ângulo
 
-  leftXAxis = map(Ps3.data.analog.stick.lx, -128, 127, 2, 181); // transforma os valores do analógico para ângulo
+    rightXAxis = map(Ps3.data.analog.stick.rx, -128, 127, 2, 181);  // transforma os valores do analógico para ângulo
 
-  Serial.print(Ps3.data.analog.stick.lx);
-  Serial.print(" || ");
-  // Serial.println(Ps3.data.analog.stick.ly);
-  // Serial.print(" || ");
-  Serial.println(leftXAxis);
+    Serial.print(Ps3.data.analog.stick.lx);
+    Serial.print(" || ");
+    // Serial.println(Ps3.data.analog.stick.ly);
+    // Serial.print(" || ");
+    Serial.print(leftXAxis);
+    Serial.print(" || ");
 
+    Serial.print(Ps3.data.analog.stick.rx);
+    Serial.print(" || ");
+    Serial.println(rightXAxis);
 
-  myservo.write(leftXAxis); //  escreve o valor convertido no Servo
+    myservoL.write(leftXAxis);  //  escreve o valor convertido no Servo
 
-  /*
+    myservoR.write(rightXAxis);  //  escreve o valor convertido no Servo
 
-  if (Ps3.data.button.cross){ //  funções com os botões do controle, pode ser qualquer coisa
-    myservo.write(177);
-  }
+    /*
 
-  if (Ps3.data.button.triangle){
-    myservo.write(2);
-  }
+    if (Ps3.data.button.cross){ //  funções com os botões do controle, pode ser qualquer coisa
+      myservo.write(177);
+    }
 
-  */
+    if (Ps3.data.button.triangle){
+      myservo.write(2);
+    }
+
+    */
 }
