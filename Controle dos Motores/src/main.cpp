7acbd9f2d0550556550bac4807cdc0969f072957
Stalkerfish <silva.allison2002@gmail.com>
2022-06-13 14:33:57 -0300

    uses the X axis of the left stick to control the servo motor

--- Controle dos Motores/src/main.cpp
+++ Controle dos Motores/src/main.cpp
@@ -1,25 +1,43 @@
-#include <Arduino.h>
-#include <ESP32Servo.h>
-#include <Ps3Controller.h>
+#include <Arduino.h>  //  framework do Arduino
+#include <ESP32Servo.h> //  biblioteca do Servo
+#include <Ps3Controller.h>  //  biblioteca do Controle de PS3
 
 Servo myservo;
 
+int leftXAxis;  //  armazena o valor do eixo X do analógico esquerdo
+
 void setup() {
     Serial.begin(9600);
-    Ps3.begin("94:b9:7e:d5:ab:e2");
+    Ps3.begin("94:b9:7e:d5:ab:e2"); //  inicializa o controle que possui o endereço MAC especificado no parâmetro
 
-    myservo.attach(27);
-    myservo.write(1);
+    myservo.attach(27); //  inicializa o Servo Motor na porta digital 27
+    myservo.write(2); //  e escreve um ângulo de 2 graus nele
 
     Serial.println("Ready.");
 }
 
 void loop() {
-  if (Ps3.data.button.cross){
-    myservo.write(178);
+
+  leftXAxis = map(Ps3.data.analog.stick.lx, -128, 127, 2, 181); // transforma os valores do analógico para ângulo
+
+  Serial.print(Ps3.data.analog.stick.lx);
+  Serial.print(" || ");
+  // Serial.println(Ps3.data.analog.stick.ly);
+  // Serial.print(" || ");
+  Serial.println(leftXAxis);
+
+
+  myservo.write(leftXAxis); //  escreve o valor convertido no Servo
+
+  /*
+
+  if (Ps3.data.button.cross){ //  funções com os botões do controle, pode ser qualquer coisa
+    myservo.write(177);
   }
 
   if (Ps3.data.button.triangle){
     myservo.write(2);
   }
+
+  */
 }
