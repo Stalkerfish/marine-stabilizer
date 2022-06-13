82be751c3814b35f2213d788cf5a15fae039451b
Stalkerfish <silva.allison2002@gmail.com>
2022-06-13 14:04:49 -0300

    first implementation of Servo <--> Ps3Controller Cross button write a large angle in the Servo; Triangle button writes minimum angle

--- Controle dos Motores/src/main.cpp
+++ Controle dos Motores/src/main.cpp
@@ -1,9 +1,25 @@
 #include <Arduino.h>
+#include <ESP32Servo.h>
+#include <Ps3Controller.h>
+
+Servo myservo;
 
 void setup() {
-  // put your setup code here, to run once:
+    Serial.begin(9600);
+    Ps3.begin("94:b9:7e:d5:ab:e2");
+
+    myservo.attach(27);
+    myservo.write(1);
+
+    Serial.println("Ready.");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (Ps3.data.button.cross){
+    myservo.write(178);
+  }
+
+  if (Ps3.data.button.triangle){
+    myservo.write(2);
+  }
 }
